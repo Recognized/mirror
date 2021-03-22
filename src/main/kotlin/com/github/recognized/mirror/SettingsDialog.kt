579f6745ad815e287526ba29b207a1eae41e5246
vladislav.saifulin <vladislav.saifulin@jetbrains.com>
2021-03-22 03:24:06 +0300

    Ui fixes

--- src/main/kotlin/com/github/recognized/mirror/SettingsDialog.kt
+++ src/main/kotlin/com/github/recognized/mirror/SettingsDialog.kt
@@ -1,27 +1,23 @@
 package com.github.recognized.mirror
 
-import com.intellij.notification.Notification
+import com.intellij.notification.NotificationDisplayType
+import com.intellij.notification.NotificationGroup
 import com.intellij.notification.NotificationType
-import com.intellij.notification.Notifications
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.ValidationInfo
-import com.intellij.openapi.ui.VerticalFlowLayout
 import com.intellij.openapi.util.Disposer
+import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBTextField
+import com.intellij.util.ui.JBUI
 import mirror.MirrorServer
 import mirror.NativeFileAccessFactory
 import mirror.tasks.ThreadBasedTaskFactory
-import org.jdesktop.swingx.action.AbstractActionExt
-import java.awt.Dimension
+import java.awt.Component
 import java.awt.event.ActionEvent
-import javax.swing.Action
-import javax.swing.JComponent
-import javax.swing.JLabel
-import javax.swing.JPanel
-import com.intellij.notification.NotificationDisplayType
-
-import com.intellij.notification.NotificationGroup
+import javax.swing.*
+import javax.swing.event.DocumentEvent
+import javax.swing.event.DocumentListener
 
 
 private val NOTIFICATION_GROUP = NotificationGroup("Mirror Server Notifications", NotificationDisplayType.BALLOON, true)
@@ -38,12 +34,31 @@ class SettingsDialog(
     }
 
     override fun createCenterPanel(): JComponent {
-        val dialogPanel = JPanel(VerticalFlowLayout())
-        val label = JLabel("Project key:")
-        dialogPanel.add(label, VerticalFlowLayout.LEFT)
+        val dialogPanel = JPanel()
+        dialogPanel.layout = BoxLayout(dialogPanel, BoxLayout.Y_AXIS)
+        val label = JBLabel("Project key:")
+        label.alignmentX = Component.LEFT_ALIGNMENT
+        field.alignmentX = Component.LEFT_ALIGNMENT
+        isOKActionEnabled = activeMirror == null
+        field.document.addDocumentListener(object : DocumentListener {
+            fun update() {
+                isOKActionEnabled = field.text != activeMirror?.projectKey
+            }
+
+            override fun insertUpdate(e: DocumentEvent?) = update()
+            override fun removeUpdate(e: DocumentEvent?) = update()
+            override fun changedUpdate(e: DocumentEvent?) = update()
+        })
+        val strut = Box.createHorizontalStrut(JBUI.pixScale(240f).toInt())
+        (strut as Box.Filler).alignmentX = Component.LEFT_ALIGNMENT
+        dialogPanel.add(label)
         dialogPanel.add(field)
-        label.preferredSize = Dimension(300, 100)
+        dialogPanel.add(strut)
         return dialogPanel
+    }
+
+    override fun getPreferredFocusedComponent(): JComponent? {
+        return field
     }
 
     override fun doValidate(): ValidationInfo? {
@@ -78,10 +93,6 @@ class SettingsDialog(
                 superAction.actionPerformed(e)
             }
         }
-    }
-
-    override fun isOKActionEnabled(): Boolean {
-        return field.text != activeMirror?.projectKey
     }
 
     companion object {
